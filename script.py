7591bdf65e78101bdca806c7a68641621f279fcc
Dan Bailey <dbailey@danbailey.net>
2024-09-09 20:36:14 -0500

    still some bugs to squash...

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 import csv
 import json
 import requests
+import sys
+
+### grab arguments (which should be archidekt deck IDs)
+archidektID = sys.argv[1]
 
 def getOracleId(cardID):
     # send the cardID to Scryfall API and return the OracleID for use in the index
@@ -27,17 +31,24 @@ def isInList(oid, mList):
     else:
         return False
 
+def howMany(oid, targetList):
+    for card in targetList:
+        if card['OracleID'] == oid:
+            return card['TotalCards']
+        else:
+            return 0
+
 ### Pull CSV file into local memory, add OracleID and create Total Card Count
+print("Loading CSV file into memory...")
 listCount = 0
 cardList = []
 with open("le.csv") as csvfile:  
     data = csv.DictReader(csvfile)
     for row in data:
-        oid = getOracleId(row['ScryfallID'])
         insertCard = {
             'Name': row['Name'],
             'ScryfallID': row['ScryfallID'],
-            'OracleID': oid,
+            'OracleID': getOracleId(row['ScryfallID']),
             'TotalCards': int(row['Number of Non-foil']) + int(row['Number of Foil'])
         }
         cardList.append(insertCard)
@@ -46,24 +57,24 @@ with open("le.csv") as csvfile:
 mergedList = []
 
 ### Let's get all the single cards into the merged list first
-final = len(cardList) - 1
+print("Creating new list with single cards...")
+final = len(cardList)
 for x in range(0, final):
     card = cardList[x]
     target = card['OracleID']
     locs = getLocations(target, cardList)
     numLocs = len(locs)
-    print (f"{card['Name']} has {numLocs} entries at {locs}.")
     if numLocs == 1:
-        print(f"Removing {card['Name']} from the list at location {x}.")
         newCard = {
             'Name': card['Name'],
-            'ScryfallID': card['ScryfallID'],
+            ## 'ScryfallID': card['ScryfallID'],
             'OracleID': card['OracleID'],
             'TotalCards': card['TotalCards']
         }
         mergedList.append(newCard)
 
 ### Let's create a merged card of multiple cards
+print("Merging duplicate cards into master list...")
 for card in cardList:
     if not isInList(card['OracleID'], mergedList):
         locs = getLocations(card['OracleID'], cardList)
@@ -80,11 +91,56 @@ for card in cardList:
             }
             mergedList.append(newCard)
 
-### Checking to see if it works as intended
-print(" ")
-print ("Merged Card List:")
 totalCardCount = 0
 for card in mergedList:
-    print(f"{card['Name']} has {card['TotalCards']} cards.")
     totalCardCount += card['TotalCards']
-print(f"Total Card Count: {totalCardCount}")
+    print(f"{card['Name']} - {card['TotalCards']}")
+print(f"Unique cards: {len(mergedList)}")
+print(f"Total cards: {totalCardCount}")
+print(f"Each card has an average of {totalCardCount / len(mergedList)} copies.")
+print("")
+### Free up memory by wiping out original card list
+cardList = []
+
+### Write the merged list to a new CSV file
+print("Writing merged list to CSV file...")
+with open('merged.csv', mode='w', newline='') as csvfile:
+    fieldnames = ['Name', 'ScryfallID', 'OracleID', 'TotalCards']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for card in mergedList:
+        writer.writerow(card)
+
+print(f"Attemping to build a decklist for Archidekt ID: {archidektID}.")
+print(" ")
+### Grab the Archidekt decklist
+jsondecklist = requests.get(f'https://archidekt.com/api/decks/{archidektID}/').json()
+decklist = jsondecklist['cards']
+totalArchidektCards = 0
+for card in decklist:
+    totalArchidektCards += card['quantity']
+
+cleanArchidekt = []
+### Start reporting re: feasibility
+print(f"Report for: {jsondecklist['name']} ({archidektID})")
+print(f"Link to Archidekt Deck: https://archidekt.com/decks/{archidektID}")
+print(f"Total Cards in Archidekt Deck: {totalArchidektCards}")
+for card in decklist:
+    # show oraclename and quantity
+    newCard = {
+        'Name': card['card']['oracleCard']['name'],
+        'OracleID': card['card']['oracleCard']['uid'],
+        'RequiredQuantity': card['quantity'],
+        'HaveQuantity': howMany(card['card']['oracleCard']['uid'], mergedList)
+    }
+    cleanArchidekt.append(newCard)
+
+haveCards = 0
+for card in cleanArchidekt:
+    # print(f"{card['Name']} - Required: {card['RequiredQuantity']} - Have: {card['HaveQuantity']}")
+    if card['HaveQuantity'] >= card['RequiredQuantity']:
+        haveCards += 1
+
+print(" ")
+print(f"You have {haveCards} out of {len(cleanArchidekt)} cards in the deck, or {haveCards / len(cleanArchidekt) * 100}%.")
+
